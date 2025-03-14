382ed7e839d112173985baeebdda3888574c2702
shoyonp <shoyonkumar143@gmail.com>
2025-03-14 06:28:50 +0600

    banner section added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -35,8 +35,8 @@ const Navbar = () => {
   );
 
   return (
-    <header className="bg-white  sticky top-0 z-50 mt-1.5">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <header className="bg-white sticky top-0 z-50 mt-1.5">
+      <div className="mx-auto max-w-7xl px-6  md:py-4 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <h1>
