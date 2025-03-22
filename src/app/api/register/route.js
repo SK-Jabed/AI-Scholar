a7671105f6537d9509db05d119d29a1b4446e429
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-03-22 23:21:50 +0600

    [Added]:Middleware, upadate user schema, when user login via social save data to db

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,17 +1,29 @@
 import { createUser } from "@/lib/utils";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export const POST = async (request) => {
-  const userData = await request.json();
-  console.log({ userData });
-  console.log(process.env.DB_URL);
-  const result = await fetch(`${process.env.DB_URL}/users`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(userData),
-  });
+  const { name, email, password, image, role } = await request.json();
 
-  console.log({ result });
+  // console.log(name, email, password, image);
+
+  // Create a DB Connection
+
+  // Encrypt The Password
+  const encryptedPassword = await bcrypt.hash(password, 5);
+
+  // Form a DB Payload
+  const userData = {
+    name,
+    email,
+    password: encryptedPassword,
+    image: image || "",
+    role: role || "student",
+  };
+
+  // Update The DB
+  const result = await createUser(userData);
+
   return new NextResponse("User successfully created", {
     status: 201,
   });
