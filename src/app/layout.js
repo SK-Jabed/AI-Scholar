fe8699b31d5d01573546482326d971bddba0ce66
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-03-22 15:46:51 +0600

    [ADDED]: Implement layout components, sidebar functionality, and new pages for contact and about sections

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import Navbar from "@/components/shared/Navbar";
 import { CardSpotlight } from "@/components/ui/CardSpotlight";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeContext";
+import { SidebarProvider } from "@/context/SidebarContext";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,19 +25,26 @@ export default function RootLayout({ children }) {
     <html lang="en" data-theme="light" suppressHydrationWarning>
       <body className={`${inter.className}`}>
         {/* Header */}
-        <Navbar />
+        {/* <Navbar /> */}
 
         {/* Main Content */}
 <<<<<<< HEAD
         <CardSpotlight>
-          <main className="min-h-screen">{children}</main>
+          <ThemeProvider>
+            <SidebarProvider>
+              <main className="min-h-screen">{children}</main>
+            </SidebarProvider>
+          </ThemeProvider>
         </CardSpotlight>
+<<<<<<< HEAD
 
 =======
         <main className="min-h-screen my-8">{children}</main>
 >>>>>>> d81d0ac ( conflict request)
+=======
+>>>>>>> eae7c02 ([ADDED]: Implement layout components, sidebar functionality, and new pages for contact and about sections)
         {/* Footer */}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
