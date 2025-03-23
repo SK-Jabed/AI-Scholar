3c376612a92bcbe7938fe53fa68611544a03718a
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-03-23 23:52:04 +0600

    middleware updated

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,1 +1,21 @@
-// Middlewares will appear here
+import { getToken } from "next-auth/jwt";
+
+export const middleware = async (request) => {
+  const { nextUrl } = request;
+  const token = await getToken({
+    req: request,
+    secret:process.env.NEXTAUTH_SECRET,
+  });
+  const isAuthenticated = !!token;
+
+  console.log(isAuthenticated, nextUrl.pathname);
+
+  if(!isAuthenticated){
+    return Response.redirect(new URL("/login", nextUrl))
+  }
+};
+
+
+export const config = {
+  matcher: "/dashboard"
+}
