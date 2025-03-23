1c68e43f8b078b75b1645235bdb04b19b646cf13
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-24 05:35:17 +0600

    [Updated]: Middleware and App sidebar's activities updated

--- src/middleware.js
+++ src/middleware.js
@@ -3,18 +3,17 @@ export const middleware = async (request) => {
   const { nextUrl } = request;
   const token = await getToken({
     req: request,
-    secret:process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
   });
   const isAuthenticated = !!token;
 
   console.log(isAuthenticated, nextUrl.pathname);
 
-  if(!isAuthenticated){
-    return Response.redirect(new URL("/login", nextUrl))
-  }
+  // if(!isAuthenticated){
+  //   return Response.redirect(new URL("/login", nextUrl))
+  // }
 };
 
-
 export const config = {
-  matcher: "/dashboard"
-}
+  matcher: "/dashboard",
+};
