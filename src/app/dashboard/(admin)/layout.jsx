b67597854956406ede7784aa65af11fbdf5eba51
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-24 08:54:54 +0600

    [ADDED & UPDATED]: Dashboard layout's necessary routes added, design updated and some file's formatted

--- src/app/dashboard/(admin)/layout.jsx
+++ src/app/dashboard/(admin)/layout.jsx
@@ -5,9 +5,7 @@ import AppHeader from "@/layout/AppHeader";
 import AppSidebar from "@/layout/AppSidebar";
 import Backdrop from "@/layout/Backdrop";
 
-export default function AdminLayout({
-  children,
-}) {
+export default function AdminLayout({ children }) {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
 
   // Dynamic class for main content margin based on sidebar state
@@ -29,7 +27,9 @@ export default function AdminLayout({
         {/* Header */}
         <AppHeader />
         {/* Page Content */}
-        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
+        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
+          {children}
+        </div>
       </div>
     </div>
   );
