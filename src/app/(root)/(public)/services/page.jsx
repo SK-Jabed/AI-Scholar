826eee2371345fad60bea00823046ce952498db5
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-25 02:20:55 +0600

    [ADDED & UPDATED]: 404 Page, Privacy Policy Page and Terms & Conditions Page added and Footer component and Services page updated

--- src/app/(root)/(public)/services/page.jsx
+++ src/app/(root)/(public)/services/page.jsx
@@ -1,18 +1,21 @@
-import LearningTool from '@/components/service/LearningTool';
-import Productivity from '@/components/service/Productivity';
-import Security from '@/components/service/Security';
-import ServiceBanner from '@/components/service/ServiceBanner';
-import React from 'react';
+import LearningTool from "@/components/service/LearningTool";
+import Productivity from "@/components/service/Productivity";
+import Security from "@/components/service/Security";
+import ServiceBanner from "@/components/service/ServiceBanner";
+import Container from "@/components/shared/Container";
+import React from "react";
 
 const Services = () => {
-    return (
-        <div>
-            <ServiceBanner/>
-            <LearningTool/>
-            <Productivity/>
-            <Security/>
-        </div>
-    );
+  return (
+    <div>
+      <ServiceBanner />
+      <Container>
+        <LearningTool />
+        <Productivity />
+        <Security />
+      </Container>
+    </div>
+  );
 };
 
 export default Services;
--- src/components/service/Security.jsx
+++ src/components/service/Security.jsx
@@ -1,33 +1,39 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Security = () => {
-     const [data, setData] = useState([])
-        useEffect(() => {
-            fetch("/json-data/security.json") // Fetch from the public folder
-              .then((response) => response.json())
-              .then((data) => setData(data))
-              .catch((error) => console.error("Error loading team data:", error));
-           
-          }, []);
-        
-    return (
-        <div>
-               <section className="py-16 px-6 ">
-      <div className="max-w-4xl mx-auto">
-        <h2 className="text-3xl font-bold  mb-10 text-center">Security & Recognition</h2>
-        <div className="grid md:grid-cols-2 gap-6">
-          {data.map((item, i) => (
-            <div key={i} className="p-6 bg-blue-50 rounded-xl shadow-md hover:shadow-lg transition">
-              <h3 className="text-xl font-semibold text-gray-800">{item.name}</h3>
-              <p className="text-gray-600 mt-2 text-sm">{item.desc}</p>
-            </div>
-          ))}
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("/json-data/security.json") // Fetch from the public folder
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error loading team data:", error));
+  }, []);
+
+  return (
+    <div>
+      <section className="">
+        <div className="">
+          <h2 className="text-3xl font-bold  mb-10 text-center">
+            Security & Recognition
+          </h2>
+          <div className="grid md:grid-cols-2 gap-6">
+            {data.map((item, i) => (
+              <div
+                key={i}
+                className="p-6 bg-blue-50 rounded-xl shadow-md hover:shadow-lg transition"
+              >
+                <h3 className="text-xl font-semibold text-gray-800">
+                  {item.name}
+                </h3>
+                <p className="text-gray-600 mt-2 text-sm">{item.desc}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-        </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Security;
