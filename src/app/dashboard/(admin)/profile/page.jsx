e3db406b2488eee0f05ef28b26170dc47d93779a
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-29 23:39:36 +0600

    [UPDATED]: User's profile page updated of client side

--- src/app/dashboard/(admin)/profile/page.jsx
+++ src/app/dashboard/(admin)/profile/page.jsx
@@ -4,15 +4,14 @@
 
 export const metadata = {
   title: "Profile | AI Scholar",
-  description:
-    "This is Profile page for the logged in user",
+  description: "This is Profile page for the logged in user",
 };
 
 export default function Profile() {
   return (
     <div>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
-        <h3 className="mb-5 text-lg font-semibold text-center mt-6 text-gray-800 dark:text-white/90 lg:mb-7">
+        <h3 className="mb-5 text-lg font-semibold text-center mt-6 text-gray-800 dark:text-white/80 lg:mb-6">
           User's profile will appear here...
         </h3>
         <div className="space-y-6">
