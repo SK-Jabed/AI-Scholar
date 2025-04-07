ec989b2428ace37c02a36b1b2f1159d8db7ae0cf
Sheikh Jabed <jabedbd2295@gmail.com>
2025-04-07 23:32:10 +0600

    [UPDATE]: Navbar and Readme markdown of AI Scholar client side updated

--- src/app/(root)/layout.js
+++ src/app/(root)/layout.js
@@ -1,15 +1,16 @@
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
+import { SessionProvider } from "next-auth/react";
 
 const layout = ({ children }) => {
   return (
     <div>
-      <Navbar />
-      {/* Main Content */}
-      <div>
-        {children}
-      </div>
-      <Footer />
+      <SessionProvider>
+        <Navbar />
+        {/* Main Content */}
+        <div>{children}</div>
+        <Footer />
+      </SessionProvider>
     </div>
   );
 };
