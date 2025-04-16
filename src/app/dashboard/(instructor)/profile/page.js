ba3feffd81282b8d20034a911f31ecae40ba01a4
rafi <rafiahmedrabby282@gmail.com>
2025-04-16 12:49:23 +0600

    popular course card , number of student

---
+++ src/app/dashboard/(instructor)/profile/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstructorProfile = () => {
+    return (
+        <div>
+            InstructorProfile
+        </div>
+    );
+};
+
+export default InstructorProfile;
