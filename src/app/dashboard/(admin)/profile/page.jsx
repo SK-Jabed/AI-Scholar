012e3f820d35cd92dcb3c570b619a33264599b27
rafi <rafiahmedrabby282@gmail.com>
2025-04-16 14:12:03 +0600

    worked on  profile

--- src/app/dashboard/(admin)/profile/page.jsx
+++ src/app/dashboard/(admin)/profile/page.jsx
@@ -7,11 +7,14 @@ export default  function Profile() {
   const { data: session } =  useSession();
 
   const userEmail = session?.user?.email;
-  console.log(userEmail)
+  // console.log(userEmail)
 
   return (
     <div>
       <div className="rounded-2xl border border-gray-200 bg-white p-6 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
+    <div className="max-w-6xl px-4 py-8">
+      <h1 className="text-2xl font-bold">Welcome, {userEmail}</h1>
+    </div>
        
         <div className="space-y-6">
           <UserMetaCard />
@@ -19,9 +22,6 @@ export default  function Profile() {
           {/* <UserAddressCard /> */}
         </div>
       </div>
-    <div className="max-w-6xl px-4 py-8">
-      <h1 className="text-2xl font-bold">Welcome, {userEmail}</h1>
-    </div>
     </div>
   );
 }
