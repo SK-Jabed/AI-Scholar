8ef25d99389419f42536f641260f031f5649d736
rafi <rafiahmedrabby282@gmail.com>
2025-04-17 13:21:25 +0600

    worked on users request

--- src/app/dashboard/(admin)/users/page.js
+++ src/app/dashboard/(admin)/users/page.js
@@ -11,17 +11,17 @@ const Users = () => {
   const { register, handleSubmit } = useForm();
 
   const axiosInstance = useAxiosInstance();
-  const {userss} = usetGetAllUsers()
+  const [data, refetch] = usetGetAllUsers()
   const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
-
+console.log(data)
 
   useEffect(() => {
-    if (userss) {
-      setUsers(userss);
+    if (data) {
+      setUsers(data);
     }
-  }, [userss]);
+  }, [data]);
   
 
 
